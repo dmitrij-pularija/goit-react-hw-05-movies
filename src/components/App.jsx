cd4cf1e6a0335154e225394ebac2d298d2f39c4e
dmitrij-pularija <dmitrij.pularija@gmail.com>
2023-02-13 15:04:28 +0200

    router bug fixed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -19,6 +19,7 @@ const App = () => {
     dataQuery('genres', 1, '');
   }, [dataQuery]);
   return (
+    <>
     <Routes>
       <Route path="/" element={<SharedLayout />}>
         <Route index element={<Home genres={genres ?? []} />} />
@@ -30,6 +31,7 @@ const App = () => {
         <Route path="*" element={<Home />} />
       </Route>
     </Routes>
+    </>
   );
 };
 
